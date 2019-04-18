a3064e5402caa631f0259fb2d3eb48208f94aa7a
Dewi Fidrianingrum <dewifidrianingrum@gmail.com>
2019-04-18 19:42:35 +0700

    Update template.php

--- application/views/Template.php
+++ application/views/Template.php
@@ -8,6 +8,7 @@
     <title>Sistem Informasi KP</title>
 
     <!-- Custom Stylesheet -->
+    
     <link rel="stylesheet" type="text/css" href="<?=base_url()?>assets/css/home.css">
 
     <!-- Bootstrap -->
@@ -25,60 +26,18 @@
     <![endif]-->
   </head>
   <body>
-    <!-- Header -->
-    <div class="container" style="background:#fff;margin-top:0px; padding-top:30px; padding-bottom:15px; border-bottom:solid thin #e8e8e8; box-shadow: 0px -6px 22px 0px rgba(0, 0, 0, 0.2); border-radius: 3px;">
-        <div class="container">
-          <div class="row">
-        <div class="col-sm-1">
-          <a href="#"><img src="<?=base_url()?>assets/img/logo.png" width="70px" style="margin-bottom: 10px"></a>
-        </div>
-        <div class="col-sm-5">
-          <div class="row">
-            <div class="col-sm-12">
-              <h3>Sistem Informasi KP</h3>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-sm-12">
-              <p><em>"Institut Teknologi Sumatera"</em></p>
-            </div>
-          </div>
-        </div>
-          <div class="row">
-            <div class="col-sm-12">
-              <p class="text-right" style="margin-top: 10px;">
-                <span class="fa-stack fa-lg">
-                  <i class="fa fa-circle fa-stack-2x"></i>
-                  <a href="#"><i class="fa fa-twitter fa-stack-1x fa-inverse"></i></a>
-                </span>
-                <span class="fa-stack fa-lg">
-                  <i class="fa fa-circle fa-stack-2x"></i>
-                  <a href="#"><i class="fa fa-facebook fa-stack-1x fa-inverse"></i></a>
-                </span>
-                <span class="fa-stack fa-lg">
-                  <i class="fa fa-circle fa-stack-2x"></i>
-                  <a href="#"><i class="fa fa-instagram fa-stack-1x fa-inverse"></i></a>
-                </span>
-                <span class="fa-stack fa-lg">
-                  <i class="fa fa-circle fa-stack-2x"></i>
-                  <a href="#"><i class="fa fa-youtube fa-stack-1x fa-inverse"></i></a>
-                </span>
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-        </div>
-    </div>
+     <!-- Header--->
+<?php $this->load->view('header'); ?>
+
   
-    <!-- Navigation -->
+    <!-- Navigation--->
     <div class="container" style="background:#daa520; height: 50px;">
         <nav>
 
         <ul>
           <?php 
       if($_SESSION['logged_in']!=1){
-        echo  "<li><a href='page_login'>Login</a></li>";   
+        echo  "<li><a href='".base_url()."index.php/KP/page_login'>Login</a></li>";   
       }else{
         echo  "<li><a href='".base_url()."index.php/KP/page_login'>Hi! ".$_SESSION['nama']."</a></li>";
       }
@@ -90,6 +49,7 @@
         </nav>
      
     </div>
+
     
     <div class="container">
       <div class="content">
@@ -168,7 +128,7 @@
     <div class="container footer">
       <div class="row">
         <div class="col-sm-12">
-          <p class="text-center">&copy; Copyright 2019 - All rights reserved.</p>
+          <p class="text-center">&copy; Copyright 2019. SiKaPe</p>
         </div>
       </div>
     </div>
