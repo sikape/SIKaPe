3b955be59cb8a5962c272310f999ff3250f7a5d2
Dewi Fidrianingrum <dewifidrianingrum@gmail.com>
2019-04-18 19:40:44 +0700

    Update daftarkp

--- application/views/mhs/DaftarKP.php
+++ application/views/mhs/DaftarKP.php
@@ -8,7 +8,9 @@
     <title>KP | INSTITUT TEKNOLOGI SUMATERA</title>
 
       <!-- Custom Stylesheet -->
-      <link rel="stylesheet" type="text/css" href="<?=base_url()?>assets/css/afterlogin.css">
+
+      <link rel="stylesheet" type="text/css" href="<?=base_url()?>assets/css/style.css">
+      <link rel="stylesheet" type="text/css" href="<?=base_url()?>assets/css/home.css">
 
     <!-- Bootstrap -->
     <link href="<?=base_url()?>assets/css/bootstrap.min.css" rel="stylesheet">
@@ -25,71 +27,9 @@
   </head>
   <body>
     <!-- Header -->
-    <div class="container" style="background:#fff;margin-top:0px; padding-top:30px; padding-bottom:15px; border-bottom:solid thin #e8e8e8; box-shadow: 0px -6px 22px 0px rgba(0, 0, 0, 0.2); border-radius: 3px;">
-        <div class="container">
-          <div class="row">
-        <div class="col-sm-1">
-          <a href="#"><img src="<?=base_url()?>assets/img/logo.png" width="70px" style="margin-bottom: 10px"></a>
-        </div>
-        <div class="col-sm-5">
-          <div class="row">
-            <div class="col-sm-12">
-              <h3>PENDAFTARAN KP ONLINE</h3>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-sm-12">
-              <p><em>"Institut Teknologi Sumatera"</em></p>
-            </div>
-          </div>
-        </div>
-        <div class="col-sm-6">
-          <div class="row">
-            <div class="col-sm-12">
-              <p class="text-right" style="margin-top: 10px;">
-                <span class="fa-stack fa-lg">
-                  <i class="fa fa-circle fa-stack-2x"></i>
-                  <a href="#"><i class="fa fa-twitter fa-stack-1x fa-inverse"></i></a>
-                </span>
-                <span class="fa-stack fa-lg">
-                  <i class="fa fa-circle fa-stack-2x"></i>
-                  <a href="#"><i class="fa fa-facebook fa-stack-1x fa-inverse"></i></a>
-                </span>
-                <span class="fa-stack fa-lg">
-                  <i class="fa fa-circle fa-stack-2x"></i>
-                  <a href="#"><i class="fa fa-instagram fa-stack-1x fa-inverse"></i></a>
-                </span>
-                <span class="fa-stack fa-lg">
-                  <i class="fa fa-circle fa-stack-2x"></i>
-                  <a href="#"><i class="fa fa-youtube fa-stack-1x fa-inverse"></i></a>
-                </span>
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-        </div>
-    </div>
-  
-    <!-- Navigation -->
-    <div class="container" style="background:#daa520; height: 50px;">
-    <nav>
-    <div class="menu-icon">
-      <i class="fa fa-bars fa-2x"></i>
-    </div>
-    <ul>
-      <li><a href="<?=base_url()?>index.php/KP/dashboard">Home</a></li>
-      <li><a href="<?=base_url()?>index.php/KP/persyaratan">Persyaratan</a></li>
-      <li><a href="<?=base_url()?>index.php/KP/panduan">Panduan Pendaftaran</a></li>
-      <li><a href="<?=base_url()?>index.php/KP/pengumuman">Pengumuman</a></li>
-      <li>Hi! <?php echo $nama; ?></li>
-    </ul>
-    </nav>
-    </div>
-  <!---->
-
-    </div>
-    </div>
+    <?php $this->load->view('header'); ?>
+    <!-- Navigation Menu -->
+    <?php $this->load->view('navigation'); ?>
 
 
       <div class="container">
@@ -137,7 +77,7 @@
           </div>
           <div class="form-group">
             <label for="No_handphone">No. Handphone :</label>
-            <input name = "No_handphone" type="number" class="form-control" required="">
+            <input name = "No_handphone" class="form-control" required="">
           </div>
           <div class="form-group">
             <label for="Email">Email :</label>
@@ -219,15 +159,11 @@
         </div> 
       </div>
       </div>
-<!---->
-    <div class="container footer">
-      <div class="row">
-        <div class="col-sm-12">
-          <p class="text-center">&copy; Copyright 2019. SiKaPe</p>
-        </div>
-      </div>
     </div>
-  
+
+      <!-- Footer -->
+
+ <?php $this->load->view('footer'); ?>
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
