4a5aa559ea4cc7f72d815aa24a6977dfc055a49c
Dewi Fidrianingrum <dewifidrianingrum@gmail.com>
2019-04-18 19:40:01 +0700

    Update Panduan

--- application/views/panduan.php
+++ application/views/panduan.php
@@ -9,7 +9,8 @@
 
     <!-- Custom Stylesheet -->
     <link rel="stylesheet" type="text/css" href="<?=base_url()?>assets/css/style.css">
-    <<link rel="stylesheet" type="text/css" href="<?=base_url()?>assets/css/home.css">
+    <link rel="stylesheet" type="text/css" href="<?=base_url()?>assets/css/home.css">
+    <link rel="stylesheet" type="text/css" href="<?=base_url()?>assets/css/panduanpendaftaran.css">
     <link href='https://fonts.googleapis.com/css?family=Lato:100,400,700' rel='stylesheet' type='text/css'>
 
     <!-- Bootstrap -->
@@ -21,81 +22,13 @@
     <!-- Social Icon Fonts -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
 
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
   </head>
   <body>
     <!-- Header -->
-    <div class="container" style="background:#fff;margin-top:0px; padding-top:30px; padding-bottom:15px; border-bottom:solid thin #e8e8e8; box-shadow: 0px -6px 22px 0px rgba(0, 0, 0, 0.2); border-radius: 3px;">
-        <div class="container">
-          <div class="row">
-        <div class="col-sm-1">
-          <a href="#"><img src="<?=base_url()?>assets/img/logo.png" width="70px" style="margin-bottom: 10px"></a>
-        </div>
-        <div class="col-sm-5">
-          <div class="row">
-            <div class="col-sm-12">
-              <h3>PENDAFTARAN KP ONLINE</h3>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col-sm-12">
-              <p><em>"Institut Teknologi Sumatera"</em></p>
-            </div>
-          </div>
-        </div>
-          <div class="row">
-            <div class="col-sm-12">
-              <p class="text-right" style="margin-top: 10px;">
-                <span class="fa-stack fa-lg">
-                  <i class="fa fa-circle fa-stack-2x"></i>
-                  <a href="#"><i class="fa fa-twitter fa-stack-1x fa-inverse"></i></a>
-                </span>
-                <span class="fa-stack fa-lg">
-                  <i class="fa fa-circle fa-stack-2x"></i>
-                  <a href="#"><i class="fa fa-facebook fa-stack-1x fa-inverse"></i></a>
-                </span>
-                <span class="fa-stack fa-lg">
-                  <i class="fa fa-circle fa-stack-2x"></i>
-                  <a href="#"><i class="fa fa-instagram fa-stack-1x fa-inverse"></i></a>
-                </span>
-                <span class="fa-stack fa-lg">
-                  <i class="fa fa-circle fa-stack-2x"></i>
-                  <a href="#"><i class="fa fa-youtube fa-stack-1x fa-inverse"></i></a>
-                </span>
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-        </div>
-    </div>
-  
+    <?php $this->load->view('header'); ?>
     <!-- Navigation -->
-    <div class="container" style="background:#daa520; height: 50px;">
-    <nav>
-    <ul>    
-    <div class="menu-icon">
-      <i class="fa fa-bars fa-2x"></i>
-    </div>
-    <ul>
-   <?php 
-      if($_SESSION['logged_in']!=1){
-        echo  "<li><a href='page_login'>Login</a></li>";   
-      }else{
-        echo  "<li><a href='".base_url()."index.php/KP/page_login'>Hi! ".$_SESSION['nama']."</a></li>";
-      }
-      ?>
-      <li><a href="<?=base_url()?>index.php/KP/panduan">Panduan Pendaftaran</a></li>
-      <li><a href="<?=base_url()?>index.php/KP/index">Home</a></li>
-
-    </ul>
-    </nav>
-    </div>
+    <?php $this->load->view('navigation'); ?>
+   
 
     <div class="container">
       <div class="row content">
@@ -195,15 +128,9 @@
         </div>
     </div>
   </div>
-
-    <div class="container footer">
-      <div class="row">
-        <div class="col-sm-12">
-          <p class="text-center">&copy; Copyright 2019</p>
-        </div>
-      </div>
-    </div>
   
+ <!-- Footer -->
+    <?php $this->load->view('Footer'); ?>
 
     <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
